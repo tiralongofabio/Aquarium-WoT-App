6167e932a3eee6a7700482fd146035e6af7e1a68
Fabio Tiralongo <fabio.tiralongo@studio.unibo.it>
2026-08-27 23:41:40 +0200

    App.AccessPoint.Navigator.Manifest

---
+++ app/src/main/java/it/uniboft/demo/aquarium_wot_app/AquariumApp.kt
@@ -0,0 +1,11 @@
+package it.uniboft.demo.aquarium_wot_app
+
+
+
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+
+@HiltAndroidApp
+class AquariumApp : Application()
--- app/src/main/java/it/uniboft/demo/aquarium_wot_app/MainActivity.kt
+++ app/src/main/java/it/uniboft/demo/aquarium_wot_app/MainActivity.kt
@@ -1,47 +1,21 @@
 package it.uniboft.demo.aquarium_wot_app
 
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import it.uniboft.demo.aquarium_wot_app.ui.theme.AquariumWoTAppTheme
+import dagger.hilt.android.AndroidEntryPoint
+import it.uniboft.aquarium.uicompose.navigation.AppNavigation
 
+
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            AquariumWoTAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            AppNavigation()
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AquariumWoTAppTheme {
-        Greeting("Android")
     }
 }
