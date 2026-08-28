ce936228835f2e626007115db36ce670d99484f4
Fabio Tiralongo <fabio.tiralongo@studio.unibo.it>
2026-08-28 18:39:34 +0200

    MultiThread.DispatchersModule

--- data/src/main/java/it/uniboft/aquarium/data/repositories/DeviceConfigRepositoryImpl.kt
+++ data/src/main/java/it/uniboft/aquarium/data/repositories/DeviceConfigRepositoryImpl.kt
@@ -1,16 +1,21 @@
 package it.uniboft.aquarium.data.repositories
 
+
 import android.content.Context
 import androidx.security.crypto.EncryptedSharedPreferences
 import androidx.security.crypto.MasterKey
 import dagger.hilt.android.qualifiers.ApplicationContext
+import it.uniboft.aquarium.data.di.IoDispatcher
 import it.uniboft.aquarium.domain.models.ApparatoConfig
 import it.uniboft.aquarium.domain.repositories.IDeviceConfigRepository
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 
 class DeviceConfigRepositoryImpl @Inject constructor(
-    @ApplicationContext private val context: Context
+    @ApplicationContext private val context: Context,
+    @IoDispatcher private val ioDispatcher: CoroutineDispatcher
 ) : IDeviceConfigRepository {
 
 
@@ -28,28 +33,34 @@ class DeviceConfigRepositoryImpl @Inject constructor(
     )
 
 
-    override fun saveConfig(config: ApparatoConfig): Result<Unit> = runCatching {
-        sharedPreferences.edit()
-            .putString("ID_APPARATO", config.idApparato)
-            .putString("TOTP_SECRET", config.totpSecret)
-            .apply()
-    }
-
-
-    override fun getConfig(): Result<ApparatoConfig?> = runCatching {
-        val id = sharedPreferences.getString("ID_APPARATO", null)
-        val secret = sharedPreferences.getString("TOTP_SECRET", null)
-
-
-        if (id != null && secret != null) {
-            ApparatoConfig(id, secret)
-        } else {
-            null
+    override suspend fun saveConfig(config: ApparatoConfig): Result<Unit> = withContext(ioDispatcher) {
+        runCatching {
+            sharedPreferences.edit()
+                .putString("ID_APPARATO", config.idApparato)
+                .putString("TOTP_SECRET", config.totpSecret)
+                .apply() // .apply() esegue in background, ma la crittografia dei dati in memoria è sincrona e pesante
         }
     }
 
 
-    override fun clearConfig(): Result<Unit> = runCatching {
-        sharedPreferences.edit().clear().apply()
+    override suspend fun getConfig(): Result<ApparatoConfig?> = withContext(ioDispatcher) {
+        runCatching {
+            val id = sharedPreferences.getString("ID_APPARATO", null)
+            val secret = sharedPreferences.getString("TOTP_SECRET", null)
+
+
+            if (id != null && secret != null) {
+                ApparatoConfig(id, secret)
+            } else {
+                null
+            }
+        }
+    }
+
+
+    override suspend fun clearConfig(): Result<Unit> = withContext(ioDispatcher) {
+        runCatching {
+            sharedPreferences.edit().clear().apply()
+        }
     }
 }
--- domain/src/main/java/it/uniboft/aquarium/domain/repositories/IDeviceConfigRepository.kt
+++ domain/src/main/java/it/uniboft/aquarium/domain/repositories/IDeviceConfigRepository.kt
@@ -1,10 +1,12 @@
 package it.uniboft.aquarium.domain.repositories
 
+
 import it.uniboft.aquarium.domain.models.ApparatoConfig
 
 
 interface IDeviceConfigRepository {
-    fun saveConfig(config: ApparatoConfig): Result<Unit>
-    fun getConfig(): Result<ApparatoConfig?>
-    fun clearConfig(): Result<Unit>
+    suspend fun saveConfig(config: ApparatoConfig): Result<Unit>
+    suspend fun getConfig(): Result<ApparatoConfig?>
+    suspend fun clearConfig(): Result<Unit>
 }
+
